98353a94a25a93c7937a42303ebe20cb5c1b83ce
alireza kargar <alirezakargarsuport@gmail.com>
2024-05-26 05:50:53 +0330

    fixed all minimals bugs

--- src/sections/robot/view/robot-view.tsx
+++ src/sections/robot/view/robot-view.tsx
@@ -4,7 +4,7 @@ import MainLayout from "src/layouts/main";
 export default function RobotView() {  
     return (
         <MainLayout>
-            hello worlddd
+            hello world
         </MainLayout>
     )
 }
