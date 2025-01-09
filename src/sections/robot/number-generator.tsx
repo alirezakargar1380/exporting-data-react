bcdd5db221f04b0151597336ba1711a58bef0dd5
alireza kargar <alirezakargarsuport@gmail.com>
2025-01-09 17:19:33 +0330

    Update number-generator.tsx

--- src/sections/robot/number-generator.tsx
+++ src/sections/robot/number-generator.tsx
@@ -10,7 +10,7 @@ export default function NumberGenerator() {
     useEffect(() => {
         const interval = setInterval(() => {
             refresh();
-        }, 10000);
+        }, 15000);
 
         // Cleanup function to clear interval when component unmounts
         return () => clearInterval(interval);
