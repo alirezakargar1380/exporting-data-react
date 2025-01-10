261b580c6c22ad1076276d1efb88afb7888c9729
alireza kargar <alirezakargarsuport@gmail.com>
2025-01-10 22:56:56 +0330

    update

--- src/sections/robot/number-generator.tsx
+++ src/sections/robot/number-generator.tsx
@@ -7,14 +7,14 @@ import { IOrderCode } from "src/types/order-code";
 export default function NumberGenerator() {
     const { order_codes, refresh } = useGetOrderCodes();
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            refresh();
-        }, 19000);
+    // useEffect(() => {
+    //     const interval = setInterval(() => {
+    //         refresh();
+    //     }, 19000);
 
-        // Cleanup function to clear interval when component unmounts
-        return () => clearInterval(interval);
-    }, []);
+    //     // Cleanup function to clear interval when component unmounts
+    //     return () => clearInterval(interval);
+    // }, []);
 
     return (
         <Box sx={{ mt: 4 }}>
